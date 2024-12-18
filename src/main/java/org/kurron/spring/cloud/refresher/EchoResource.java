156b422724301313e8651e9c74f6a7c5e38e0267
rkurr <rkurr@jvmguy.com>
2024-12-18 20:14:55 +0000

    Can log the caller's ip address.

--- src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
+++ src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
@@ -1,5 +1,6 @@
 package org.kurron.spring.cloud.refresher;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -16,9 +17,18 @@ public class EchoResource {
     private static final Logger logger = LoggerFactory.getLogger(EchoResource.class);
 
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
-    public String currentTime() {
-        var now = Instant.now().toString();
-        logger.info("currentTime returning {}", now);
-        return now;
+    public String currentTime(HttpServletRequest request) {
+        var where = determineCallerAddress(request);
+        var when = Instant.now().toString();
+        logger.info("currentTime called from {} at {}", where, when);
+        return when;
+    }
+
+    private String determineCallerAddress(HttpServletRequest request) {
+        var ipAddress = request.getHeader("X-Forwarded-For");
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+        return ipAddress;
     }
 }
