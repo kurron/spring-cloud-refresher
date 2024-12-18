29725a1f02e4d7df170160aef7b0195716b693a2
rkurr <rkurr@jvmguy.com>
2024-12-18 20:21:52 +0000

    Can audit the call to the database.

--- src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
+++ src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
+import java.time.OffsetDateTime;
+
+import static java.time.ZoneOffset.UTC;
 
 @RestController
 @RequestMapping(path="/echo")
@@ -16,12 +18,20 @@ public class EchoResource {
 
     private static final Logger logger = LoggerFactory.getLogger(EchoResource.class);
 
+    private final AuditRepository repository;
+
+    public EchoResource(AuditRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     public String currentTime(HttpServletRequest request) {
         var where = determineCallerAddress(request);
-        var when = Instant.now().toString();
+        var when =  OffsetDateTime.now(UTC);
         logger.info("currentTime called from {} at {}", where, when);
-        return when;
+        var saved = repository.save(new AuditEntity(null, where, when));
+        logger.info("Audit record {} saved", saved.id());
+        return when.toString();
     }
 
     private String determineCallerAddress(HttpServletRequest request) {
