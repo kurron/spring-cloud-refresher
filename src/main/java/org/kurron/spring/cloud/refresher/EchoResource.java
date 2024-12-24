e37ff92fc8ae0ea9d4fd77330953b9ca691bffd9
rkurr <rkurr@jvmguy.com>
2024-12-24 16:25:52 +0000

    Each instance is now randomly assigned an identifier, making it easier to observe different instances handling the workload.

--- src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
+++ src/main/java/org/kurron/spring/cloud/refresher/EchoResource.java
@@ -3,6 +3,7 @@ package org.kurron.spring.cloud.refresher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,11 @@ public class EchoResource {
 
     private final AuditRepository repository;
 
-    public EchoResource(AuditRepository repository) {
+    private final String instanceId;
+
+    public EchoResource(AuditRepository repository, @Value("${spring.application.id}") String instanceId) {
         this.repository = repository;
+        this.instanceId = instanceId;
     }
 
     @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
@@ -31,12 +35,12 @@ public class EchoResource {
         logger.info("currentTime called from {} at {}", where, when);
         var saved = repository.save(new AuditEntity(null, where, when));
         logger.info("Audit record {} saved", saved.id());
-        return when.toString();
+        return "Instance " + instanceId + " serviced a call from " + where + " @ " + when;
     }
 
     private String determineCallerAddress(HttpServletRequest request) {
         var ipAddress = request.getHeader("X-Forwarded-For");
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+        if (null == ipAddress || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
             ipAddress = request.getRemoteAddr();
         }
         return ipAddress;
