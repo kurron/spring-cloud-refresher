393988053526c5536146c59b72cde4bf1187e36c
rkurr <rkurr@jvmguy.com>
2024-12-27 16:37:39 +0000

    Completed the S3 testing. On to something else.

--- src/test/java/org/kurron/spring/cloud/refresher/SpringCloudRefresherApplicationTests.java
+++ src/test/java/org/kurron/spring/cloud/refresher/SpringCloudRefresherApplicationTests.java
@@ -1,21 +1,26 @@
 package org.kurron.spring.cloud.refresher;
 
-import io.awspring.cloud.autoconfigure.s3.properties.S3Properties;
+import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Operations;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static java.time.ZoneOffset.UTC;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -27,37 +32,50 @@ class SpringCloudRefresherApplicationTests {
     @Autowired
     S3Operations s3;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     LocalStackConnectionDetails connectionDetails;
 
-    @Autowired
-    S3Properties s3Properties;
-
     @Test
+    @DisplayName("Make sure the application stands up")
     void contextLoads() {
     }
 
-    @BeforeEach
-    void beforeEachTest() {
-        s3Properties.setEndpoint(connectionDetails.endpoint());
-    }
-
     @Test
-    void testS3() {
-        assertNotNull(s3Properties);
+    @DisplayName("Exercise S3 calls")
+    void testS3() throws IOException {
         assertNotNull(connectionDetails);
         assertNotNull(s3);
-        var exists = s3.bucketExists(Long.toHexString(ThreadLocalRandom.current().nextLong(Long.MAX_VALUE)));
-        assertFalse(exists, "Should never exist!");
+        var bucket = Long.toHexString(ThreadLocalRandom.current().nextLong(Long.MAX_VALUE));
+        assertFalse( s3.bucketExists(bucket), "Should never exist!");
+        assertNotNull(s3.createBucket(bucket), "Bucket was not created!");
+        byte[] buffer = new byte[256];
+        ThreadLocalRandom.current().nextBytes(buffer);
+        var key = Long.toHexString(ThreadLocalRandom.current().nextLong(Long.MAX_VALUE));
+        var resource = s3.upload(bucket, key, new ByteArrayInputStream(buffer), ObjectMetadata.builder().contentType("application/octet-stream").build());
+        assertTrue(resource.exists(), "Resource should exist!");
+        assertEquals("application/octet-stream", resource.contentType());
+        assertEquals(bucket, resource.getLocation().getBucket());
+        try( var in = resource.getInputStream() ) {
+            try ( var out = new ByteArrayOutputStream() ) {
+                out.write(in.readAllBytes());
+                byte[] read = out.toByteArray();
+                assertArrayEquals(read, buffer, "Bytes read from the bucket do NOT match bytes written to the bucket!");
+            }
+        }
+        s3.deleteObject(bucket, key);
+        s3.deleteBucket(bucket);
+        assertFalse(s3.bucketExists(bucket), "Bucket should have been deleted!");
     }
 
     @Test
+    @DisplayName("Exercise Repositories")
     void loadAuditRecords() {
         assertNotNull(auditRepository);
         var saved = auditRepository.save(new AuditEntity(null, "192.168.1.1", OffsetDateTime.now(UTC)));
         assertEquals(1, saved.id());
         var found = auditRepository.findById(saved.id());
-        //nano seconds are slightly different so ignore them for the comparison
+        //nanoseconds are slightly different so ignore them for the comparison
         assertEquals(saved.when().toLocalDateTime().toEpochSecond(ZoneOffset.MIN), found.orElseThrow().when().toLocalDateTime().toEpochSecond(ZoneOffset.MIN));
     }
 }
