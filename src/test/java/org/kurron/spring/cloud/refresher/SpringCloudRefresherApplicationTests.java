bf40a4a0bbd7639bbfcd4457cb1749fcdfd1890c
rkurr <rkurr@jvmguy.com>
2024-12-30 21:27:03 +0000

    Fleshing out the DynamoDB tests.

--- src/test/java/org/kurron/spring/cloud/refresher/SpringCloudRefresherApplicationTests.java
+++ src/test/java/org/kurron/spring/cloud/refresher/SpringCloudRefresherApplicationTests.java
@@ -8,12 +8,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+import software.amazon.awssdk.services.dynamodb.model.TableClass;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static java.time.ZoneOffset.UTC;
@@ -36,6 +49,9 @@ class SpringCloudRefresherApplicationTests {
     @Autowired
     DynamoDbOperations dynamoDb;
 
+    @Autowired
+    DynamoDbClient dynamoDbClient;
+
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     LocalStackConnectionDetails connectionDetails;
@@ -45,11 +61,84 @@ class SpringCloudRefresherApplicationTests {
     void contextLoads() {
     }
 
+    // Records are not supported so we have to go old school and use a class with mutators
+    @DynamoDbBean
+    public static class Person {
+        String id; // partition key
+        String firstName; // not part of the ky
+        String lastName; // sort key
+
+        public Person() {
+            // required for Spring
+        }
+
+        public Person(String id, String firstName, String lastName) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        @DynamoDbPartitionKey
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, firstName, lastName);
+        }
+    }
+
     @Test
     @DisplayName("Exercise DynamoDB calls")
     void testDynamoDB() {
         assertNotNull(connectionDetails);
         assertNotNull(dynamoDb);
+        assertNotNull(dynamoDbClient);
+        // a "row" is an Item, each Item contains Attributes (bits of data), Hash/Partition key index to an Item, Range/Sort sorts/limits Partition keys
+        // scalar type stores 1 value (string, number, binary, boolean)
+        // set type stores multiple scalars (string set, number set, binary set) in unordered fashion
+        // document type (list is ordered array, map is JSON)
+        var table = "person"; // default behavior is snake case of the entity
+        var createResponse = dynamoDbClient.createTable(CreateTableRequest.builder().tableName(table)
+                                                                                    .keySchema(KeySchemaElement.builder().attributeName("id").keyType(KeyType.HASH).build(),KeySchemaElement.builder().attributeName("lastName").keyType(KeyType.RANGE).build())
+                                                                                    .attributeDefinitions(AttributeDefinition.builder().attributeName("id").attributeType(ScalarAttributeType.S).build(),AttributeDefinition.builder().attributeName("lastName").attributeType(ScalarAttributeType.S).build())
+                                                                                    .provisionedThroughput(ProvisionedThroughput.builder().readCapacityUnits(1L).writeCapacityUnits(1L).build())
+                                                                                    .build());
+        assertEquals(table, createResponse.tableDescription().tableName(), "Table names don't match!");
+        var toSave = new Person(UUID.randomUUID().toString(), "Ron", "Kurr");
+        var saved = dynamoDb.save(toSave);
+        var allItems = dynamoDb.scanAll(Person.class);
+        assertEquals(saved, allItems.stream().findFirst().orElseThrow().items().stream().findFirst().orElseThrow(), "Item was not found!");
+        //Person loaded = dynamoDb.load(Key.builder().partitionValue(saved.id.toString()).build(), Person.class);
         var i = 0;
     }
 
